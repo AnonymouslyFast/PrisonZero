e8130c5f796408b4ea8bf2d6fef24798a364fcd9
AnonymouslyFast <102996231+AnonymouslyFast@users.noreply.github.com>
2025-11-22 23:40:44 -0500

    Created World Data management.

--- src/main/java/com/anonymouslyfast/Main.java
+++ src/main/java/com/anonymouslyfast/Main.java
@@ -1,8 +1,10 @@
 package com.anonymouslyfast;
 
+import com.anonymouslyfast.game.data.DataManager;
 import com.anonymouslyfast.game.player.listeners.JoinListener;
 import com.anonymouslyfast.game.world.Generators.FlatWorldGenerator;
 import com.anonymouslyfast.game.world.World;
+import com.anonymouslyfast.game.world.WorldManager;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.event.Event;
 import net.minestom.server.event.EventNode;
@@ -14,15 +16,14 @@ public final class Main {
     private static final String serverAddress = "0.0.0.0";
     private static final int serverPort = 25565; // Default mc port
 
+    private static final DataManager serverDataManager = new DataManager("Databases/server.db");
+    private static final WorldManager worldManager = new WorldManager(serverDataManager);
+
     static void main(String[] args) {
         MinecraftServer minecraftServer = MinecraftServer.init();
 
-        World world = new World(new FlatWorldGenerator())
-                .enableLighting(true)
-                .enableSaveWorld(true)
-                .generate();
-
-        registerPlayerEvents(world);
+        World activeWorld = registerWorldSystem();
+        registerPlayerEvents(activeWorld);
 
         minecraftServer.start(serverAddress, serverPort);
     }
@@ -33,4 +34,32 @@ public final class Main {
 
         MinecraftServer.getGlobalEventHandler().addChild(playerNode);
     }
+
+    static World registerWorldSystem() {
+        worldManager.getWorldDataManager().loadWorlds();
+
+        // Saves worlds on shutdown
+        MinecraftServer.getSchedulerManager().buildShutdownTask(() -> {
+            MinecraftServer.getInstanceManager().getInstances().forEach(instance -> {
+                instance.saveChunksToStorage().join();
+            });
+        });
+
+        World world = null;
+        // Generating world in first pos, if there's one.
+        if (worldManager.getAllWorlds().length > 0) {
+            world = worldManager.getAllWorlds()[0];
+            world.generate();
+        // Creating a default world if there's no worlds.
+        } else if (worldManager.getAllWorlds().length == 0) {
+           world = new World(new FlatWorldGenerator())
+                    .setName("defaultWorld")
+                    .enableSaveWorld(true)
+                    .enableLighting(true)
+                    .generate();
+            worldManager.registerWorld(world);
+        }
+        return world;
+    }
+
 }
